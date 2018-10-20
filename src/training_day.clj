7ba0423bfe901373025901c9472febe9c538ee8b
Nate <nhays@ycp.edu>
2018-10-19 20:34:58 -0400

    finish?

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,11 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer(str (42)))
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn square [x] 
+  (fn[x] (* x x)))
 
 (defn average [a b]
-  ":(")
+  (fn[x] (/ 2 (+ a b))))
